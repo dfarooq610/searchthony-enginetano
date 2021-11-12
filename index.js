f853e27b1b7af6bb0c3737cd34092c682d15f4ef
albeartoz <zhang.alb@northeastern.edu>
2021-11-11 22:44:37 -0500

    Our first attempt at using youtube API

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-require('dotenv').config()
+require("dotenv").config();
+const { google } = require("googleapis");
+const youtube = google.youtube({
+  version: "v3",
+  auth: process.env.API_KEY,
+});
+
+const getVideos = async () => {
+  return await youtube.search.list(
+    {
+      part: "snippet",
+      channelId: "UCt7fwAhXDy3oNFTAzF2o8Pw",
+      order: "date",
+      maxResults: 3715,
+    },
+    function (err, data) {
+      if (err) {
+        console.error("Error: " + err);
+      }
+      return data.request.responseURL;
+    }
+  );
+  //   const res = await fetch(resLink.request.responseURL, {
+  //     method: "GET",
+  //   });
+  //   console.log(resLink);
+};
+
+getVideos().then((data) => {
+  console.log(data);
+});
+//getChannel()
