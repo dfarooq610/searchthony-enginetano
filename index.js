ac438b39a3a4060ba51bd37336fa42a089b4562b
dfarooq610 <dfarooq@Danishs-MacBook-Pro.local>
2022-02-12 18:53:26 -0500

    filter then map

--- index.js
+++ index.js
@@ -8,30 +8,33 @@ let videoList = []
 do {
   const response = await fetch(
     "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet%2CcontentDetails&maxResults=50&pageToken=" +
-      pageToken +
-      "&playlistId=" +
-      process.env.UPLOADS_PLAYLIST +
-      "&key=" +
-      process.env.API_KEY
+    pageToken +
+    "&playlistId=" +
+    process.env.UPLOADS_PLAYLIST +
+    "&key=" +
+    process.env.API_KEY
   );
   const data = await response.json();
   pageToken = data["nextPageToken"];
 
-  videoList  = [...videoList, ...data["items"]
+  videoList = [...videoList, ...data["items"]
+    .filter(
+      item => {
+        let { title } = item["snippet"];
+        title = title.toLowerCase()
+        return title.includes(" review") &&
+        !title.toLowerCase().includes("y u no") &&
+        !title.toLowerCase().includes("track")
+      }
+    )
     .map(item => {
       const { snippet, contentDetails } = item;
       const { title, description, thumbnails } = snippet;
       const { videoId, videoPublishedAt } = contentDetails;
       const { url } = thumbnails["high"] ? thumbnails["high"] : thumbnails["default"];
 
-      return { title, description, videoPublishedAt, videoURL: `https://www.youtube.com/watch?v=${videoId}`, thumbnailURL: url};
-    })
-    .filter(
-      item =>
-        item.title.toLowerCase().includes(" review") &&
-        !item.title.toLowerCase().includes("y u no") && 
-        !item.title.toLowerCase().includes("track")
-    )];
+      return { title, description, videoPublishedAt, videoURL: `https://www.youtube.com/watch?v=${videoId}`, thumbnailURL: url };
+    })];
 } while (pageToken !== "" && pageToken !== undefined);
 
 fs.writeFile("output3.json", JSON.stringify(videoList), (err) => {
