7376cfd6a76eed73669731543b255dd086af9eb1
albeartoz <zhang.alb@northeastern.edu>
2022-02-12 16:25:39 -0500

    Adds getting a page of reviews

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+import "dotenv/config";
+import fetch from "node-fetch";
+import * as fs from "fs";
 
+const response = await fetch(
+  "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet%2CcontentDetails&maxResults=50&pageToken=EAAaBlBUOkNESQ&playlistId=" +
+    process.env.UPLOADS_PLAYLIST +
+    "&key=" +
+    process.env.API_KEY
+);
+const data = await response.json();
+
+const videoList = data["items"]
+  .map((item) => {
+    const { snippet } = item;
+    const { title, description } = snippet;
+    return { title, description };
+  })
+  .filter((item) => item.title.toLowerCase().includes(" review"));
+
+console.log(data);
+fs.writeFile("output2.json", JSON.stringify(videoList), (err) => {
+  console.log(err);
+});
