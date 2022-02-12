bf9579f6777059188a4f89297dbad47b7177ea22
albeartoz <zhang.alb@northeastern.edu>
2022-02-12 18:08:49 -0500

    adds initial scraping functionality

--- index.js
+++ index.js
@@ -2,23 +2,37 @@ import "dotenv/config";
 import fetch from "node-fetch";
 import * as fs from "fs";
 
-const response = await fetch(
-  "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet%2CcontentDetails&maxResults=50&pageToken=EAAaBlBUOkNESQ&playlistId=" +
-    process.env.UPLOADS_PLAYLIST +
-    "&key=" +
-    process.env.API_KEY
-);
-const data = await response.json();
+let pageToken = "";
+let videoList = []
 
-const videoList = data["items"]
-  .map((item) => {
-    const { snippet } = item;
-    const { title, description } = snippet;
-    return { title, description };
-  })
-  .filter((item) => item.title.toLowerCase().includes(" review"));
+do {
+  const response = await fetch(
+    "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet%2CcontentDetails&maxResults=50&pageToken=" +
+      pageToken +
+      "&playlistId=" +
+      process.env.UPLOADS_PLAYLIST +
+      "&key=" +
+      process.env.API_KEY
+  );
+  const data = await response.json();
+  pageToken = data["nextPageToken"];
 
-console.log(data);
-fs.writeFile("output2.json", JSON.stringify(videoList), (err) => {
-  console.log(err);
+  videoList  = [...videoList, ...data["items"]
+    .map((item) => {
+      const { snippet } = item;
+      const { title, description } = snippet;
+      return { title, description };
+    })
+    .filter(
+      (item) =>
+        item.title.toLowerCase().includes(" review") &&
+        !item.title.toLowerCase().includes("y u no")
+    )];
+} while (pageToken != "" && pageToken != null);
+
+fs.writeFile("output3.json", JSON.stringify(videoList), (err) => {
+  if (err != null) {
+    console.log(err);
+  }
 });
+// console.log(data);
